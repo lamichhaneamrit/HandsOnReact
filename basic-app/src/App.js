737fe91602255ae87ee288c9c832e3e158a75a5c
Amrit Lamichhane <lami.amrit@gmail.com>
2022-12-15 22:29:29 +0100

    FirstApp2

---
+++ basic-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <h1>Welcome To Frontend Development</h1>
+    </div>
+  );
+}
+
+export default App;
